ded1eedfa319a2e42e51502be1d9936e43824f71
Soham <76859008+Flecha248@users.noreply.github.com>
2024-08-29 11:35:02 +0530

    Update set2q2.c

--- Sem 3/DSA/set2q2.c
+++ Sem 3/DSA/set2q2.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure of a node
 struct Node
 {
     int data;
     struct Node *next;
 };
 
-// Function to create a new node
 struct Node *createNode(int data)
 {
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
@@ -17,7 +15,6 @@ struct Node *createNode(int data)
     return newNode;
 }
 
-// Function to create a circular linked list with n nodes
 struct Node *createCircularLinkedList(int n)
 {
     struct Node *head = NULL, *temp = NULL, *newNode = NULL;
@@ -35,36 +32,33 @@ struct Node *createCircularLinkedList(int n)
             temp = temp->next;
         }
     }
-    temp->next = head; // Make the list circular
+    temp->next = head; 
     return head;
 }
 
-// Function to solve the Josephus problem using circular linked list
 int josephus(struct Node *head, int k)
 {
     struct Node *temp = head;
     struct Node *prev = NULL;
 
     while (temp->next != temp)
-    { // Only one node remains in the list
-        // Move to the k-th node
+    { 
+       
         for (int count = 1; count < k; count++)
         {
             prev = temp;
             temp = temp->next;
         }
 
-        // Remove the k-th node
         printf("Person at position %d is eliminated.\n", temp->data);
         prev->next = temp->next;
         free(temp);
         temp = prev->next;
     }
 
-    return temp->data; // The last remaining node
+    return temp->data; 
 }
 
-// Main function
 int main()
 {
     int n, k;
