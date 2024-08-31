380497d25bb77480b12925134cd6516901cccc05
Soham <76859008+Flecha248@users.noreply.github.com>
2024-08-30 23:39:17 -0700

    Update a1q4.asm

--- Sem 3/ASM/a1q4.asm
+++ Sem 3/ASM/a1q4.asm
@@ -10,17 +10,18 @@ mov si,0030h
 mov di,0050h
 mov bx,0000h
 mov [di],bx
-
+mov cx,0070h
 looping:
 	mov bl, [si]
 	add [di], bl
 	mov bl,00h
-	adc bl,bl
 	inc di
 	add [di],bl
 	dec di
 	inc si
 loop looping
+
+mov ah,03h
 mov ah,4ch
 int 21h
 main endp
