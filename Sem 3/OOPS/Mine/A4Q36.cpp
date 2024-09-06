99bc15275d0d922eabe02267176a73a38441e515
Soham <76859008+Flecha248@users.noreply.github.com>
2024-09-06 02:32:14 -0700

    Create A4Q36.cpp

---
+++ Sem 3/OOPS/Mine/A4Q36.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+class Point
+{
+        double x, y;
+        public:
+                Point(double x = 0, double y = 0)
+                {
+                        this->x=x;
+                        this->y=y;
+                }
+
+        ~Point()
+        {
+                cout<<"Point destroyed"<<endl;
+        }
+
+
+        void setX(double x)
+        {       this->x=x;
+        }
+
+        void setY(double y)
+        {       this->y=y;
+        }
+
+
+        double operator-(Point &ob)
+        {
+                double dx = x - ob.x;
+                double dy = y - ob.y;
+
+                return sqrt(dx*dx + dy*dy);
+        }
+};
+
+        void setX(double x)
+        {       this->x=x;
+        }
+
+        void setY(double y)
+        {       this->y=y;
+        }
+
+
+        double operator-(Point &ob)
+        {
+                double dx = x - ob.x;
+                double dy = y - ob.y;
+
+                return sqrt(dx*dx + dy*dy);
+        }
+};
+
+int main() {
+    Point p1(3.0, 4.0);
+    Point p2(0.0, 0.0);
+
+   cout << "Distance: " << (p1 - p2) << endl;
+
+    return 0;
+}
