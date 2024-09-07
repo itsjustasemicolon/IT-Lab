2b3170f2b0259d0770cbf9243e70c5930c26d97a
Soham <76859008+Flecha248@users.noreply.github.com>
2024-09-07 00:00:17 -0700

    Create a1q11.asm

---
+++ Sem 3/ASM/a1q11.asm
@@ -0,0 +1,27 @@
+dosseg
+.model small
+.stack 100h
+.data
+.code
+
+main proc
+
+mov ax,@data
+mov ds,ax
+mov si,0040h
+mov ax,[si]
+mov si,0050h
+mov bx,[si]
+div bx
+
+mov si,0060h
+mov [si],ax
+mov si,0062h
+mov [si],dx
+
+int 03h
+mov ah,4ch
+int 21h
+
+main endp
+end main
