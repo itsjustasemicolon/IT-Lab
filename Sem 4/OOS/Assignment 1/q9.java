c9c710e43342113b6b06c834f1aac53185e226e7
itsjustasemicolon <76859008+itsjustasemicolon@users.noreply.github.com>
2025-01-13 16:27:41 +0530

    Update q9.java

--- Sem 4/OOS/Assignment 1/q9.java
+++ Sem 4/OOS/Assignment 1/q9.java
@@ -13,12 +13,9 @@ class Person {
         this.address = address;
     }
 
-    public void printPersonDetails() {
-        System.out.println("Age: " + age);
-        System.out.println("Weight: " + weight);
-        System.out.println("Height: " + height);
-        System.out.println("Date of Birth: " + dateOfBirth);
-        System.out.println("Address: " + address);
+    public void printDetails() {
+        System.out.println("Age: " + age + ", Weight: " + weight + ", Height: " + height +
+                ", Date of Birth: " + dateOfBirth + ", Address: " + address);
     }
 }
 
@@ -35,11 +32,9 @@ class Employee extends Person {
         this.experience = experience;
     }
 
-    public void printEmployeeDetails() {
-        printPersonDetails();
-        System.out.println("Salary: " + salary);
-        System.out.println("Date of Joining: " + dateOfJoining);
-        System.out.println("Experience: " + experience + " years");
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Salary: " + salary + ", Date of Joining: " + dateOfJoining + ", Experience: " + experience + " years");
     }
 }
 
@@ -62,26 +57,15 @@ class Student extends Person {
             average += mark;
         }
         average /= marks.length;
-        
         if (average >= 90) return "A";
         else if (average >= 75) return "B";
         else if (average >= 50) return "C";
         else return "F";
     }
 
-    public void printStudentDetails() {
-        printPersonDetails();
-        System.out.println("Roll Number: " + roll);
-        System.out.print("Subjects: ");
-        for (String subject : listOfSubjects) {
-            System.out.print(subject + " ");
-        }
-        System.out.println();
-        System.out.print("Marks: ");
-        for (int mark : marks) {
-            System.out.print(mark + " ");
-        }
-        System.out.println("\nGrade: " + calculateGrade());
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Roll Number: " + roll + ", Grade: " + calculateGrade());
     }
 }
 
@@ -91,58 +75,56 @@ class Technician extends Employee {
         super(age, weight, height, dateOfBirth, address, salary, dateOfJoining, experience);
     }
 
-    public void printTechnicianDetails() {
-        printEmployeeDetails();
-        System.out.println("Technician is ready to perform technical tasks.");
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Technician ready for technical tasks.");
     }
 }
 
 class Professor extends Employee {
     private String[] courses;
-    private String[] listOfAdvisees;
+    private String[] advisees = new String[0];
 
     public Professor(int age, double weight, double height, String dateOfBirth, String address,
                      double salary, String dateOfJoining, int experience, String[] courses) {
         super(age, weight, height, dateOfBirth, address, salary, dateOfJoining, experience);
         this.courses = courses;
-        this.listOfAdvisees = new String[0]; 
     }
 
     public void addAdvisee(String adviseeName) {
-        String[] newList = new String[listOfAdvisees.length + 1];
-        System.arraycopy(listOfAdvisees, 0, newList, 0, listOfAdvisees.length);
-        newList[listOfAdvisees.length] = adviseeName;
-        listOfAdvisees = newList;
+        String[] newAdvisees = new String[advisees.length + 1];
+        System.arraycopy(advisees, 0, newAdvisees, 0, advisees.length);
+        newAdvisees[advisees.length] = adviseeName;
+        advisees = newAdvisees;
     }
 
     public void removeAdvisee(String adviseeName) {
         int index = -1;
-        for (int i = 0; i < listOfAdvisees.length; i++) {
-            if (listOfAdvisees[i].equals(adviseeName)) {
+        for (int i = 0; i < advisees.length; i++) {
+            if (advisees[i].equals(adviseeName)) {
                 index = i;
                 break;
             }
         }
         if (index != -1) {
-            String[] newList = new String[listOfAdvisees.length - 1];
-            for (int i = 0, j = 0; i < listOfAdvisees.length; i++) {
+            String[] newAdvisees = new String[advisees.length - 1];
+            for (int i = 0, j = 0; i < advisees.length; i++) {
                 if (i != index) {
-                    newList[j++] = listOfAdvisees[i];
+                    newAdvisees[j++] = advisees[i];
                 }
             }
-            listOfAdvisees = newList;
+            advisees = newAdvisees;
         }
     }
 
-    public void printProfessorDetails() {
-        printEmployeeDetails();
-        System.out.print("Courses taught: ");
+    public void printDetails() {
+        super.printDetails();
+        System.out.print("Courses: ");
         for (String course : courses) {
             System.out.print(course + " ");
         }
-        System.out.println();
-        System.out.print("Advisees: ");
-        for (String advisee : listOfAdvisees) {
+        System.out.println("\nAdvisees: ");
+        for (String advisee : advisees) {
             System.out.print(advisee + " ");
         }
         System.out.println();
@@ -154,22 +136,22 @@ public class Main {
 
         String[] subjects = {"Math", "Physics", "Chemistry"};
         int[] marks = {85, 90, 78};
-        Student student = new Student(20, 65, 1.75, "01/01/2003", "123 Street", "S123", subjects, marks);
-        student.printStudentDetails();
+        Student student = new Student(20, 65, 1.75, "01/01/2023", "123 Park Street", "S123", subjects, marks);
+        student.printDetails();
         System.out.println();
 
-        Technician technician = new Technician(30, 70, 1.80, "15/05/1993", "456 Avenue", 45000, "01/06/2015", 8);
-        technician.printTechnicianDetails();
+        Technician technician = new Technician(30, 70, 1.80, "15/05/1999", "456 Java Avn", 45000, "01/06/2015", 8);
+        technician.printDetails();
         System.out.println();
 
-        String[] courses = {"Computer Science", "AI"};
-        Professor professor = new Professor(45, 80, 1.85, "10/02/1978", "789 Boulevard", 75000, "01/01/2005", 20, courses);
+        String[] courses = {"OOS", "AI/ML"};
+        Professor professor = new Professor(45, 80, 1.85, "10/02/1978", "789 Santoshpur", 75000, "01/01/2005", 20, courses);
         professor.addAdvisee("Alice");
         professor.addAdvisee("Bob");
-        professor.printProfessorDetails();
+        professor.printDetails();
 
         professor.removeAdvisee("Alice");
         System.out.println("After removal:");
-        professor.printProfessorDetails();
+        professor.printDetails();
     }
 }
