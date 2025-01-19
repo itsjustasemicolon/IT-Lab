8c70033e6cfa01ddf493924488b67810bc5fb368
itsjustasemicolon <76859008+itsjustasemicolon@users.noreply.github.com>
2025-01-19 20:36:16 +0530

    Update q5.java

--- Sem 4/OOS/Assignment 1/q5.java
+++ Sem 4/OOS/Assignment 1/q5.java
@@ -1,33 +1,34 @@
-class Complex
-{
-    float re,im;
-    Complex()
-    {
-        re=0;
-        im=0;
+class Complex {
+    float real, imaginary;
+
+    Complex() {
+        real = 0;
+        imaginary = 0;
     }
-    Complex(float r)
-    {
-        re=r;
-        im=0;
+
+    Complex(float r) {
+        real = r;
+        imaginary = 0;
     }
-    Complex(float r, float i)
-    {
-        re=r;
-        im=i;
+
+    Complex(float r, float i) {
+        real = r;
+        imaginary = i;
     }
-    void add(Complex c)
-    {
-        System.out.println("Sum: "+(re+c.re)+"+ ("+(im+c.im)+"i)");
+
+    void add(Complex c) {
+        System.out.println("Sum: " + (real + c.real) + "+ (" + (imaginary + c.imaginary) + "i)");
     }
-    void mul(Complex c)
-    {
-        System.out.println("Product: "+(re*c.re-im*c.im)+"+ ("+(re*c.im+im*c.re)+"i)");
+
+    void mul(Complex c) {
+        System.out.println("Product: " + (real * c.real - imaginary * c.imaginary) + "+ (" + (real * c.imaginary + imaginary * c.real) + "i)");
     }
-    public static void main(String args[])
-    {
-        Complex c1=new Complex(3,3);
-        Complex c2=new Complex(4,-2);
+}
+
+class q6 {
+    public static void main(String[] args) {
+        Complex c1 = new Complex(3, 2);
+        Complex c2 = new Complex(4, -2);
         c1.add(c2);
         c1.mul(c2);
     }
