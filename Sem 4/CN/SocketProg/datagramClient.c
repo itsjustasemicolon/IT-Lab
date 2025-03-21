30f9d1a3c0318ee78cabdbf35e8e1fefbbaea45f
itsjustasemicolon <76859008+itsjustasemicolon@users.noreply.github.com>
2025-03-21 10:57:03 +0530

    Create datagramClient.c

---
+++ Sem 4/CN/SocketProg/datagramClient.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define BUFFER_SIZE 256
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, n;
+    struct sockaddr_in serv_addr;
+    socklen_t addr_len = sizeof(serv_addr);
+    char buffer[BUFFER_SIZE];
+   
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    int PORT = atoi(argv[1]);
+
+    // Create UDP socket (changed to SOCK_DGRAM)
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        error("ERROR opening socket");
+    }
+
+    // Setup server address (same as before)
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        error("Invalid address");
+    }
+
+    while (1) {
+        printf("Enter message: ");
+        bzero(buffer, BUFFER_SIZE);
+        fgets(buffer, BUFFER_SIZE, stdin);
+        
+        // Send using sendto() instead of send()
+        if (sendto(sockfd, buffer, strlen(buffer), 0, 
+                  (struct sockaddr *)&serv_addr, addr_len) < 0) {
+            error("Send failed");
+        }
+        
+        // Receive using recvfrom() instead of read()
+        bzero(buffer, BUFFER_SIZE);
+        n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, 
+                    (struct sockaddr *)&serv_addr, &addr_len);
+        
+        if (n > 0) {
+            printf("Server: %s", buffer);
+        } else {
+            break;
+        }
+
+        if (strncmp("bye", buffer, 3) == 0) {
+            break;
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}
