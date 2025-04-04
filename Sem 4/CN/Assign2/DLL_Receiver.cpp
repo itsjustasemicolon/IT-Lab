6c68c895381390556ef097603d263ea8038ff05f
Soham Das <76859008+itsjustasemicolon@users.noreply.github.com>
2025-04-04 11:31:35 +0530

    Update DLL_Receiver.cpp

--- Sem 4/CN/Assign2/DLL_Receiver.cpp
+++ Sem 4/CN/Assign2/DLL_Receiver.cpp
@@ -1,112 +1,49 @@
 #include <iostream>
+#include <fstream>
 #include <cstring>
-#include <chrono>
-#include <thread>
-#include <cstdlib>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <unistd.h> // For sleep()
 
-#define FRAME_SIZE 1024
+using namespace std;
 
-void WaitForEvent();
-void ReceiveFrame(int connfd, char* frame);
-void ExtractData(char* frame, char* data);
-void DeliverData(char* data);
+struct Frame {
+    char data[256];
+};
 
-void WaitForEvent()
-{
-    std::cout << "Waiting for frame..." << std::endl;
-    int wait_time = 500 + std::rand() % 1500;
-    std::this_thread::sleep_for(std::chrono::milliseconds(wait_time));
+bool WaitForEventReceiver() {
+    ifstream file("medium.dat", ios::binary);
+    return file.good();
 }
 
-void ReceiveFrame(int connfd, char* frame)
-{
-    int n = read(connfd, frame, FRAME_SIZE);
-    if (n < 0) {
-        std::cerr << "Error in receiving frame" << std::endl;
-        exit(EXIT_FAILURE);
+Frame ReceiveFrame() {
+    Frame frame;
+    ifstream inFile("medium.dat", ios::binary);
+    if(inFile) {
+        inFile.read(reinterpret_cast<char*>(&frame), sizeof(Frame));
+        inFile.close();
     }
-    frame[n] = 0;
+    return frame;
 }
 
-void ExtractData(char* frame, char* data)
-{
-    strncpy(data, frame, FRAME_SIZE);
+char* ExtractData(Frame frame) {
+    static char data[256];
+    strncpy(data, frame.data, sizeof(data));
+    return data;
 }
 
-void DeliverData(char* data)
-{
-    std::cout << "Received frame: " << data << std::endl;
+void DeliverData(char* data) {
+    cout << "Received message: " << data << endl;
 }
 
-void Receiver(char const* ip, int port)
-{
-    int sockfd, connfd;
-    struct sockaddr_in servaddr, cli;
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        std::cerr << "Socket creation failed." << std::endl;
-        exit(EXIT_FAILURE);
+int main() {
+    while(true) {
+        cout << "Receiver waiting for messages... (Ctrl+C to quit)" << endl;
+        if (WaitForEventReceiver()) {
+            Frame received_frame = ReceiveFrame();
+            char* received_data = ExtractData(received_frame);
+            DeliverData(received_data);
+            remove("medium.dat");
+        }
+        sleep(1); // Check every second
     }
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port   = htons(port);
-
-    if (inet_pton(AF_INET, ip, &servaddr.sin_addr) <= 0) {
-        std::cerr << "Invalid address / Address not supported" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if ((bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) != 0) {
-        std::cerr << "Socket bind failed." << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if ((listen(sockfd, 1)) != 0) {
-        std::cerr << "Listen failed." << std::endl;
-        exit(EXIT_FAILURE);
-    } else {
-        std::cout << "Listening for connections..." << std::endl;
-    }
-
-    socklen_t len = sizeof(cli);
-
-    connfd = accept(sockfd, (struct sockaddr*)&cli, &len);
-    if (connfd < 0) {
-        std::cerr << "Server accept failed." << std::endl;
-        exit(EXIT_FAILURE);
-    } else {
-        std::cout << "Connection established." << std::endl;
-    }
-
-    char frame[FRAME_SIZE];
-    char data[FRAME_SIZE];
-
-    while (true) {
-        WaitForEvent();
-        ReceiveFrame(connfd, frame);
-        if (!frame[0]) break;
-        ExtractData(frame, data);
-        DeliverData(data);
-    }
-
-    close(connfd);
-    close(sockfd);
-}
-
-int main(int argc, char* argv[])
-{
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <IP> <Port>" << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    char const* ip = argv[1];
-    int port       = std::stoi(argv[2]);
-
-    Receiver(ip, port);
     return 0;
 }
